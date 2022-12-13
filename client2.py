635920d143aa70b649ea3ad8ca9745cdd110974a
Jason Tsao Wei Yu <weiyu.tsao@gmail.com>
2022-12-13 15:47:16 -0800

    Update client2.py

--- client2.py
+++ client2.py
@@ -1,7 +1,7 @@
 import paho.mqtt.client as mqtt
 import time,logging
 broker="broker.hivemq.com"
-home_topic="client_b"
+home_topic="weiyu"
 port=1883
 QOS=0
 
@@ -35,7 +35,7 @@ def on_message(client, userdata, message):
 def on_publish(client, userdata, mid):
     logging.info("message published "  +str(mid))
 
-send_topic ="eecs149/client_a/" +home_topic
+send_topic ="house/xuezhe/" +home_topic
 client= mqtt.Client("ClientB",False)       #create client object
 
 client.on_subscribe = on_subscribe   #assign function to callback
@@ -45,7 +45,7 @@ client.on_message=on_message
 client.connect(broker,port)           #establish connection
 time.sleep(1)
 client.loop_start()
-client.subscribe("eecs149/eecs149group3")
+client.subscribe("house/eecs149group3")
 count=1
 while True: #runs forever break with CTRL+C
 
