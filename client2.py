c151882896f2f6df799e4bf31ea93718c212e463
Jason Tsao Wei Yu <weiyu.tsao@gmail.com>
2022-12-13 17:16:28 -0800

    Update client2.py

--- client2.py
+++ client2.py
@@ -4,6 +4,7 @@ camera = PiCamera()
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 import time,logging
+import base64
 broker="broker.hivemq.com"
 home_topic="weiyu"
 port=1883
@@ -39,11 +40,14 @@ def on_message(client, userdata, message):
           camera.capture("2nd_pi's_camera_picture.jpg")
           f= open("2nd_pi's_camera_picture.jpg","rb")
           content = f.read()
-          mybyteArray = bytearray(content)
+          #mybyteArray = bytearray(content)
           #g=open('receivedimage.jpg','wb')
           #g.write(mybyteArray)
           #g.close()
-          publish.single(home_topic,mybyteArray,hostname="mqtt.eclipseprojects.io")
+          encoded = base64.b64encode(content)
+          f.close()
+          client.publish(home_topic, encoded,hostname="mqtt.eclipseprojects.io")
+          #publish.single(home_topic,mybyteArray,hostname="mqtt.eclipseprojects.io")
           #2nd photo from 2nd pi
           #camera.capture("2nd_pi's_camera_picture.jpg")
           #f= open("2nd_pi's_camera_picture.jpg")
