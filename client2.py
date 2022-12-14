b8aa26cb68fe56c0db9e8b4f9b947e9af5984048
Jason Tsao Wei Yu <weiyu.tsao@gmail.com>
2022-12-13 18:49:10 -0800

    Update client2.py

--- client2.py
+++ client2.py
@@ -1,7 +1,38 @@
-import paho.mqtt.publish as publish
+from picamera import PiCamera
+camera = PiCamera()
+import paho.mqtt.client as mqtt
 
-f= open("2nd_pi's_camera_picture.jpg","rb")
-filecontent = f.read()
-byteArr = bytearray(filecontent)
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+  print("Connected with result code "+str(rc))
+  # Subscribing in on_connect() means that if we lose the connection and
+  # reconnect then subscriptions will be renewed.
+  client.subscribe("topiceecs149part1")
+  
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+  print ('msg received')
+  camera.capture("2nd_pi's_camera_picture.jpg")
+  f= open("2nd_pi's_camera_picture.jpg","rb")
+  filecontent = f.read()
+  byteArr = bytearray(filecontent)
+  publish.single('topiceecs149part2', byteArr, qos=1, hostname='broker.hivemq.com')
+  
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("broker.hivemq.com", 1883, 60)
 
-publish.single('topiceecs149', byteArr, qos=1, hostname='broker.hivemq.com')
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
+
+
+
+
+
+
+
+
